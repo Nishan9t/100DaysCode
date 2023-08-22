8a413dffe3d3854e7064de42313c534b5862894a
Nishan9t <108941493+Nishan9t@users.noreply.github.com>
2023-08-22 10:00:27 +0530

    day25 final commit
    problem links and solutions of day25 questions

---
+++ DAY25/permutation-sequence.java
@@ -0,0 +1,34 @@
+//   https://leetcode.com/problems/permutation-sequence
+
+
+
+class Solution {
+    public String getPermutation(int n, int k) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        int fact =1;
+        for(int i=1;i<n;i++)
+        {
+            fact=fact*i;
+            arr.add(i);
+        }
+        arr.add(n);
+        //beacuse arrylist runs from 0 index
+        k=k-1;
+        String ans="";
+        while(true)
+        {
+            int i1=k/fact;
+            int i2=k%fact;
+            ans+=arr.get(i1);
+            arr.remove(i1);
+            k=i2;
+            if(arr.size()==0)
+            {
+                break;
+            }
+            fact =fact/arr.size();
+        }
+        return ans;
+        
+    }
+}
