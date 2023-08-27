ec092b4c9536df793b3d9f3b1aaab0edb1009e64
Nishan9t <108941493+Nishan9t@users.noreply.github.com>
2023-08-27 15:52:42 +0530

    day30 ques 1 solved

---
+++ DAY30/all-paths-from-source-to-target.java
@@ -0,0 +1,28 @@
+//   https://leetcode.com/problems/all-paths-from-source-to-target
+
+
+class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+      List<List<Integer>> allPaths = new ArrayList<List<Integer>>();
+      List<Integer> list = new ArrayList<>();
+      list.add(0);
+      backtracking(allPaths,list,0,graph);
+      return allPaths;
+        
+    }
+    public void backtracking(List<List<Integer>> allPaths, List<Integer> list, int index, int[][] graph)
+    {
+        if(index==graph.length-1)
+        {
+            allPaths.add(new ArrayList<>(list));
+            return;
+        }
+        for(int i=0;i<graph[index].length;i++)
+        {
+            list.add(graph[index][i]);
+           // backtracking to index = graph[index][i] means going to node which just added in list to check the further path from that node
+            backtracking(allPaths,list,graph[index][i],graph);
+            list.remove(Integer.valueOf(graph[index][i]));
+        }
+    }
+}
