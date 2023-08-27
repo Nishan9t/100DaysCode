743d75c06d194646a214c9aba257effe2a98503a
Nishan9t <108941493+Nishan9t@users.noreply.github.com>
2023-08-27 15:53:44 +0530

    day 30 final commit
    problem links and solutions of day30 questions

---
+++ DAY30/house-robber-iii.java
@@ -0,0 +1,43 @@
+//   https://leetcode.com/problems/house-robber-iii
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rob(TreeNode root) {
+        int[] num= dfs(root);
+        return Math.max(num[0],num[1]);
+        
+    }
+    private int[] dfs(TreeNode x)
+    {
+        if(x==null)
+        {
+            return new int[2];
+        }
+        int[] left = dfs(x.left);
+        int[] right=dfs(x.right);
+        int[] res=new int[2];
+        //max value if rob this node
+        res[0]=left[1]+right[1]+x.val;
+        //max value if not rob this node
+        res[1]=Math.max(left[0],left[1])+Math.max(right[0],right[1]);
+        return res;
+    }
+}
+
+
