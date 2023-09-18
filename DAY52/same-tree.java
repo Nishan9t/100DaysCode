f3d44486a2d875449ed165f6a74a58f6cc8ddaaf
Nishant Pandey <108941493+Nishan9t@users.noreply.github.com>
2023-09-18 11:36:48 +0530

    two solved questions of day52

---
+++ DAY52/same-tree.java
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/same-tree
+
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null)
+        {
+            return true;
+        }
+        if(p!=null && q==null)
+        {
+            return false;
+        }
+        if(p==null && q!=null)
+        {
+            return false;
+        }
+        if(p.val!=q.val)
+        {
+            return false;
+        }
+//if both nodes are same 
+        boolean l= isSameTree(p.left,q.left);
+        boolean r=  isSameTree(p.right,q.right);
+        
+        return l&r;
+    }
+}
