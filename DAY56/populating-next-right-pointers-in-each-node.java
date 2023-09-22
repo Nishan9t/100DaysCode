c2e9eae9fedc9d85e3e8f1279061b8b10dec6461
Nishant Pandey <108941493+Nishan9t@users.noreply.github.com>
2023-09-22 12:37:58 +0530

    final commit of day56
    problem links and solutions of day56 questions

---
+++ DAY56/populating-next-right-pointers-in-each-node.java
@@ -0,0 +1,62 @@
+// https://leetcode.com/problems/populating-next-right-pointers-in-each-node
+
+
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+class Solution {
+    public Node connect(Node root) {
+        if(root == null)
+        {
+            return null;
+        }
+        Queue<Node> q= new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty())
+        {
+            int size = q.size();
+            for(int i=0;i<size ; i++)
+            {
+                Node node = q.poll();
+                if(i<size-1)
+                {
+                    node.next = q.peek();
+                }
+                if(node.left != null)
+                {
+                    q.offer(node.left);
+                }
+                if(node.right !=null)
+                {
+                    q.offer(node.right);
+                }
+            }
+        }
+        return root;
+        
+    }
+}
+
+
+
+
+
+
