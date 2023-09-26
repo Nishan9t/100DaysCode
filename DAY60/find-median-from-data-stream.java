bf4108126ed378408abd9fa874fcae2e0d3ef1ef
Nishant Pandey <108941493+Nishan9t@users.noreply.github.com>
2023-09-26 11:37:01 +0530

    two solved questions of day60

---
+++ DAY60/find-median-from-data-stream.java
@@ -0,0 +1,48 @@
+// https://leetcode.com/problems/find-median-from-data-stream
+
+
+
+class MedianFinder {
+    ArrayList<Integer> list;
+    public MedianFinder() {
+        list = new ArrayList<>();
+    }
+    
+    public void addNum(int num) {
+        int i;
+        if(list.size()>0)
+        {
+            for(i=0;(i<list.size() && list.get(i)<num);i++);
+            
+            list.add(i,num);
+          
+        }  
+        else
+        {
+                list.add(num);
+        }
+        
+    }
+    
+    public double findMedian() {
+        int index = list.size()/2;
+        if(list.size() %2 ==0)
+        {
+            return (double)(list.get(index)+list.get(index-1))/2;
+        }
+        else
+        {
+            return list.get(index);
+        }
+        
+    }
+}
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
+
+
+
