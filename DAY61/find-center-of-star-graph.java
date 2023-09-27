cecf41776c931ade90ffa44e0ca640fded68daff
Nishant Pandey <108941493+Nishan9t@users.noreply.github.com>
2023-09-27 10:30:07 +0530

    final commit of day61 with problem links and solutions

---
+++ DAY61/find-center-of-star-graph.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/find-center-of-star-graph
+
+
+
+class Solution {
+    public int findCenter(int[][] edges) {
+        int a = edges[0][0];
+        int b = edges[0][1];
+        int c = edges[1][0];
+        int d = edges[1][1];
+        if(a==c) return a;
+        if(a==d) return a;
+        if(b==c) return c;
+        if(b==d) return b;
+        return 0;
+       
+    }
+}
+
