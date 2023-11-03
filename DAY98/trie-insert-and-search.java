e7714c5fcd7a2a2d66ed49846ba0a94ba8f92a9c
Nishant Pandey <108941493+Nishan9t@users.noreply.github.com>
2023-11-03 10:06:05 +0530

    two solved questions of day98

---
+++ DAY98/trie-insert-and-search.java
@@ -0,0 +1,74 @@
+// https://leetcode.com/problems/implement-trie-prefix-tree
+
+
+
+class TrieNode{
+  boolean isWord;
+  TrieNode[] children;
+  public TrieNode(){
+      isWord=false;
+      children = new TrieNode[26];
+  }
+}
+
+
+class Trie {
+   TrieNode root;
+  public Trie() {
+     root = new TrieNode();
+
+  }
+
+  public void insert(String word) {
+      TrieNode node =root;
+      for(char c : word.toCharArray())
+      {
+          int index = c-'a';
+          if(node.children[index]  == null)
+          {
+              node.children[index] =new TrieNode();
+          }
+          node = node.children[index];
+      }
+      node.isWord = true;
+
+  }
+
+  public boolean search(String word) {
+      TrieNode node = root;
+      for(char c: word.toCharArray())
+      {
+          int index = c -'a';
+          if(node.children[index] == null)
+          {
+              return false;
+          }
+          node = node.children[index];
+      }
+      return node.isWord;
+
+  }
+
+  public boolean startsWith(String prefix) {
+
+      TrieNode node = root;
+      for (char c : prefix.toCharArray()) {
+          int index = c - 'a';
+          if (node.children[index] == null) {
+              return false;
+          }
+          node = node.children[index];
+      }
+      return true;
+
+  }
+}
+/**
+* Your Trie object will be instantiated and called as such:
+* Trie obj = new Trie();
+* obj.insert(word);
+* boolean param_2 = obj.search(word);
+* boolean param_3 = obj.startsWith(prefix);
+*/
+
+
